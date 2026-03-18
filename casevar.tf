82aa3177f230317b8f9ce5b8dfdfe8d5cf05d7cc
Abiramavalli98 <abiramaavlli98@gmail.com>
2026-03-18 00:38:54 +0000

    Initial terraform commit

---
+++ casevar.tf
@@ -0,0 +1,12 @@
+variable "project-id" {
+  default = "abi-project-431802"
+}
+variable "zone" {
+  default = "us-central1-a"
+}
+variable "machine_type" {
+  default = "e2-medium"
+}
+variable "instance_name" {
+  default = "terraform-instance"
+}
